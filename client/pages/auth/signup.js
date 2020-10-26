d2c7ed40ac5b4226c9e27b5e283d7f71adcfda43
lzm0010 <lzm0010@gmail.com>
2020-10-26 09:56:34 -0500

    added next.js client

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <form>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input className="form-control"/>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input type="password"className="form-control"/>
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  )
+}
---
+++ client/pages/index.js
@@ -0,0 +1,3 @@
+export default () => {
+  return <h1>LaNDing page</h1>;
+}
